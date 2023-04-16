04b6a4989f14b5eb6fe999c6f4d143d8c03c12bd
Dim0chka <Bugrick2004@yandex.ru>
2023-04-17 02:24:38 +0300

    new

---
+++ src/components/NavMap.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="wrapper_flex">
+    <ul class="list">
+        <li v-for="item in arr" :id="item.id" @mouseout="isNotHover($event.target.id)" @mouseover="isHover($event.target.id)" :class="item.class" :key="item.id">
+            {{ item.name }}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        MapId: {
+            type: String
+        }
+    },
+    data() {
+        return {
+            arr: [
+                {id: 1, name: 'Лабораторный корпус', class: 'lab'},
+                {id: 2, name: "Конгресс центр", class: 'Congress'},
+                {id: 3, name: "Главный корпус", class: 'Main'},
+                {id: 4, name: "Музей истории электросвязи", class: 'Museum'},
+                {id: 5, name: "ДИСиТ", class: 'Disit'},
+                {id: 6, name: "Библиотечный корпус", class: 'library'},
+                {id: 7, name: "УЛК(Учебно-лабораторный корпус)", class: 'ULK'}
+            ]
+        }
+    },
+    methods: {
+        isHover(e) {
+            this.$emit('getNavId', e)
+            const newArr = this.arr.filter(el => el.id != e)
+
+            newArr.forEach(e => {
+                document.querySelector(`.${e.class}`).classList.add('back')
+            })
+
+        },
+
+        isNotHover(e) {
+            this.$emit('getNavId', "")
+            const newArr = this.arr.filter(el => el.id != e)
+
+            newArr.forEach(e => {
+                document.querySelector(`.${e.class}`).classList.remove('back')
+            })
+        },
+
+    },
+    updated() {
+        if (this.MapId != "") {
+            const newArr = this.arr.filter(el => el.id != this.MapId)
+
+            newArr.forEach(e => {
+                document.querySelector(`.${e.class}`).classList.add('back')
+            })
+        } else {
+            const newArr = this.arr.filter(el => el.id != this.MapId)
+
+            newArr.forEach(e => {
+                document.querySelector(`.${e.class}`).classList.remove('back')
+            })
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,53 @@
+<template>
+  <navbar @search="getSearch"></navbar>
+  <div class="container">
+    <div class="main__wrap">
+      <MainMap
+        :NavId="NavId"
+        :curSearch="curSearch"
+        @getMapId="getMapId"
+      >
+      </MainMap>
+      <NavMap 
+        :MapId="MapId"
+        @getNavId="getNavId"
+      >
+      </NavMap>
+    </div>
+  </div>
+</template>
+
+<script>
+import navbar from '@/components/navbar.vue';
+import MainMap from '@/components/MainMap.vue';
+import NavMap from '@/components/NavMap.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    MainMap, navbar, NavMap
+  },
+  data() {
+    return {
+      curSearch: '',
+      MapId: 0,
+      NavId: 0
+    }
+  },
+  methods: {
+    getSearch(SearchQuery) {
+      this.curSearch = SearchQuery
+    },
+    getMapId(e) {
+      this.MapId = e
+    },
+    getNavId(e) {
+      this.NavId = e
+    }
+    // getIdSearch(e) {
+    //   this.id = e
+    //   console.log(e)
+    // }
+  }
+}
+</script>
